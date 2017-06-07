60f717e583c208e27d93b6642470312deafb00db
MoreNaruto <morenaruto007@gmail.com>
2017-06-06 20:24:10 -0500

    Working on first exercise

---
+++ src/Application.java
@@ -0,0 +1,11 @@
+import Exercises.ArrayAndListExercises;
+
+/**
+ * Created by tmosq on 6/6/17.
+ */
+public class Application {
+    public static void main(String[] args){
+        ArrayAndListExercises arrayAndListExercises = new ArrayAndListExercises();
+
+    }
+}
---
+++ src/Exercises/ArrayAndListExercises.java
@@ -0,0 +1,14 @@
+package Exercises;
+
+/**
+ * Created by tmosq on 6/6/17.
+ */
+public class ArrayAndListExercises {
+
+    public ArrayAndListExercises() {
+    }
+
+    public Boolean allUniqueCharacters(String word){
+        return false;
+    }
+}
